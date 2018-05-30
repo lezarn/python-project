0f80b87cba13d93c2ab4be64be203b0f39222f0f
lezarn <32242491+lezarn@users.noreply.github.com>
2018-05-30 10:34:15 +0800

    new version

---
+++ p0529/db.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+
+class db_controler:
+    pass
+    
+	
+    def __init__(self):
+        self.database = sqlite3.connect("database.db")
+        self.cur = self.database.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS user(account text, password text,
+        authority integer,token text,ltime integer, PRIMARY KEY(account))''')
+
+        if(self.user_search("root")==None):
+            self.user_insert("root","password",0)
+		
+		
+		
+    def user_insert(self, user_ac, user_pw, user_auth = 2,token = "",ltime = 0):
+        info = (user_ac, user_pw ,user_auth,token,ltime)
+        self.cur.execute("INSERT INTO user VALUES (?,?,?,?,?)", info)
+        self.database.commit()
+		
+    def user_search(self, user_ac):
+        info = (user_ac,)
+        self.cur.execute("SELECT ltime FROM user WHERE account=?", info)
+        self.database.commit()
+        return self.cur.fetchone()
+    
+    def user_login(self, user_ac, user_pw):
+        info = (user_ac, user_pw)
+        self.cur.execute("SELECT * FROM user WHERE account=? AND password=?", info)
+        self.database.commit()
+        return self.cur.fetchone()
+
+    def user_search_token(self, token):
+        info = (token,)
+        self.cur.execute("SELECT * FROM user WHERE token=?", info)
+        self.database.commit()
+        return self.cur.fetchone()
+
+    def user_search_time(self, token):
+        info = (token,)
+        self.cur.execute("SELECT ltime FROM user WHERE token=?", info)
+        self.database.commit()
+        return self.cur.fetchone()
+
+    def user_update(self, token, ltime, user_ac):
+        info = (token, ltime, user_ac)
+        self.cur.execute("UPDATE user SET token=?, ltime=? WHERE account=?",info)
+
+    def user_update_token(self, token, ltime):
+        info = (token, ltime, token)
+        self.cur.execute("UPDATE user SET token=?, ltime=? WHERE token=?",info)
+
+
+    def user_delete(self, user_ac):
+        info = (user_ac,)
+        self.cur.execute("DELETE FROM user WHERE account=?", info)
+        self.database.commit()
+
+
+    def print_all(self):
+        self.cur.execute("SELECT * FROM user ORDER BY authority")
+        self.database.commit()
+        return print(self.cur.fetchall())
+    
+
+    def delete_all(self):
+        self.cur.execute("DELETE FROM user")
+        self.database.commit()
+        
+	
+    def __del__(self):
+        self.database.close()
+		
+	
